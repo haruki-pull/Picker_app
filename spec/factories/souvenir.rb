5c6fa941f94cc8326d0a7826f8f0b8df44e50169
harukipull <harukigit@gmail.com>
2019-11-16 16:59:31 +0900

    Update request_spec.rb when_add_souvenir_request

--- spec/factories/souvenir.rb
+++ spec/factories/souvenir.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :souvenir, class: 'Souvenir' do 
+  factory :pass_souvenir, class: Souvenir do 
     name {"MyString"}
     spot {"MyString"}
     price {1}
@@ -7,4 +7,14 @@ FactoryBot.define do
     genre {"MyString"}
     comment {"MyString"}
   end
+
+  factory :failure_souvenir, class: Souvenir do
+    name {" "}
+    spot {" "}
+    price { }
+    picture {" "}
+    genre {" "}
+    comment{"美味しい"}
+  end
+  
 end
--- spec/requests/request_spec.rb
+++ spec/requests/request_spec.rb
@@ -3,50 +3,44 @@ require 'rails_helper'
 RSpec.describe 'POST /souvenirs', type: :request do
     
     context '全てのパラメーターが揃っているとき' do
-        it '200 OKをだす' do
-           post '/souvenirs', params: {name: MyString,
-                                        spot: MyString,
-                                        price: 1,
-                                        picture: MyString,
-                                        genre: MyString,
-                                        comment: MyString}
-            expect(response).to have_http_status(:ok)
+
+        before(:each)   do
+            post '/souvenirs', params: { souvenir: FactoryBot.attributes_for(:pass_souvenir) }
         end
-        it 'ユーザー登録'
-        it 'ユーザー登録成功のflash'
+
+        it 'リクエスト成功' do
+            expect(response.status).to eq 302
+        end
+        
+        it 'ユーザー登録' do
+            expect {
+                post '/souvenirs', params: { souvenir: FactoryBot.attributes_for(:pass_souvenir) }
+            }.to change(Souvenir, :count).by(1)
+        end
+
+        it 'ユーザー登録成功のflash' do
+            expect(flash[:success]).not_to be_empty
+        end
+
     end
 
     context 'パラメーターが欠けていたとき' do
-        it '400 Bad Requestをだす'
-        it 'ユーザー登録失敗'
-        it 'ユーザー登録失敗のflash'
+        
+        before(:each) do
+            post '/souvenirs', params: {souvenir: FactoryBot.attributes_for(:failure_souvenir)}
+        end
+        #ユーザー登録失敗してもstatus codeは200なのでテストしない
+
+        it 'ユーザー登録失敗' do
+            expect {
+                post '/souvenirs', params: {souvenir: FactoryBot.attributes_for(:failure_souvenir)}
+            }.to change(Souvenir, :count).by(0)
+        end
+
+        it 'ユーザー登録失敗のflash' do
+            expect(response.body).to include("箇所の不備が存在します")
+        end
+
     end
 
 end
-
-<%
-=begin
-%>
-
-let(:url) { '/souvenirs/show' }
-        let(:souvenir) {  create(:souvenir, name:'a',spot: 'a') }
-        let(:params) do
-            {
-                id: souvenir.id,
-                access_token: 'aaaaaaaaaaaaa' 
-            }
-        end
-    
-        # 'do {} end == {{}}' 中の{}はハッシュです。紛らわしいですが。
-        # ここまででurlとparamsを作成しました。
-    
-        it '200 success' do
-          get url, params
-          expect(response).to have_http_status 200
-        end
-    
-        # urlとparamsをgetメソッドに渡すと成功します。
-      end
-<%
-=begin
-%>
