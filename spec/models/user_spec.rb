fb92ea8366d299e5b07facc0fa465535f851bac6
harukipull <harukigit@gmail.com>
2019-11-28 00:23:08 +0900

    add rspec_test to users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "ユーザーが生成されるか"
+  it "ユーザーが生成されるか" do
+    user = FactoryBot.create(:user)
+    expect(user).to be_valid
+  end
 end
