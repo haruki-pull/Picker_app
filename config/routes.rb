7c9888094edfda2965f56b72724b9ebed4b1db6d
harukipull <harukigit@gmail.com>
2019-12-01 12:51:40 +0900

    add login template

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
 root 'souvenirs#index'
-get '/post', to: 'souvenirs#new'
 
-#get '/login',to: 'sessions/new'
+get '/post',   to: 'souvenirs#new'
+get '/login',  to: 'sessions#new'
+post '/login', to: 'sessions#create'
+get '/logout', to: 'sessions#destroy'
 resources :souvenirs
 resources :users
 end
