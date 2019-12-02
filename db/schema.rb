541aa145878968f5123f95d4298488d56298eceb
harukipull <harukigit@gmail.com>
2019-12-02 20:52:08 +0900

    AddUserRefToSouvenirs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_26_072914) do
+ActiveRecord::Schema.define(version: 2019_12_02_114617) do
 
   create_table "souvenirs", force: :cascade do |t|
     t.string "name"
@@ -22,7 +22,9 @@ ActiveRecord::Schema.define(version: 2019_11_26_072914) do
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["name"], name: "index_souvenirs_on_name"
+    t.index ["user_id"], name: "index_souvenirs_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
-
-20.times do 
-      name = Faker::Name.name
-      spot = Faker::Nation.capital_city
-      price = Faker::Number.within(range: 1000..3000)
-      genre = Faker::Job.field
-      comment = Faker::Quote.robin 
-
-Souvenir.create!(name: name,
-                 spot: spot,
-                 price: price,
-                 genre: genre,
-                 picture: File.open("./app/assets/images/rs_siroikoi2.jpg"),
-                 comment: comment) 
-end
-
 20.times do 
       name = Faker::Name.name
       email = Faker::Internet.email
@@ -24,4 +8,18 @@ end
 User.create!(name: name,
              email:email,
              password_digest:"foobar")
+end
+
+users = User.order(:id).take(6)
+20.times do 
+      name = Faker::Name.name
+      spot = Faker::Nation.capital_city
+      price = Faker::Number.within(range: 1000..3000)
+      genre = Faker::Job.field
+      comment = Faker::Quote.robin 
+users.each { |user| user.souvenirs.create!(name: name,
+                                           spot: spot,
+                                           price: price,
+                                           genre: genre,
+                                           comment: comment) }
 end
