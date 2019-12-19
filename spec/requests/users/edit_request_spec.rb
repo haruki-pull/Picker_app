fcbbc4c4e3b0b235659d29c0215fab7000242bb5
harukipull <harukigit@gmail.com>
2019-12-19 19:26:55 +0900

    clean edit_request_spec

--- spec/requests/users/edit_request_spec.rb
+++ spec/requests/users/edit_request_spec.rb
@@ -3,24 +3,18 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :request do
     
     describe 'POST /users' do
+
         context do
-
             let(:user) { FactoryBot.create(:user) }    
-            # before 'ユーザーIDをセッションから取り出せるようにする' do
-            #     allow_any_instance_of(ActionDispatch::Request)
-            #       .to receive(:session).and_return(user: user)
-            # end
-
+            before do
+                allow_any_instance_of(ActionDispatch::Request).to receive(:session).and_return(user_id: user.id)
+            end
                 it 'リクエスト成功' do
                     get user_url user.id
-                    expect(response.status).to eq 302
+                    expect(response.status).to eq 200
                 end
 
                 it '@userの取得' do
-                #rspec内でsessionを使う方法探す
-                    post :create, session: {user_id: 1}
-                    #post :create, session: {user_id: user.id}
-                    login(user)
                     get user_url user.id
                     expect(response.body).to include "MyString"
                 end
